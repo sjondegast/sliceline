1dcee0b2a7b8fb83fab066b5e6622ea119b47c3c
Sjon de Gast <sjondegast@gmail.com>
2020-02-17 17:49:59 +0100

    added note to Link component

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Link.scss';
 
 // TODO: className via props
+// TODO: @readme usage with react-router import link as routerlink from 'react-router-dom'
 
 const Link = ({ href = '/', children = 'link', className }) => (
 	<a className={className} href={`${href}`}>
