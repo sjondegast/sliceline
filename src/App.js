f04258514696f2a1c234ed918ea1674359698711
Sjon de Gast <sjondegast@gmail.com>
2020-02-21 21:24:05 +0100

    added index.js to Navbar this saves with importing, need to import inside curly braces because it isn't an default export

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar';
+import { Navbar } from './components/Navbar';
 import Link from './components/Link/Link';
 import { Heading, Text } from './components/Typography';
 import Banner from './components/Banner/Banner';
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './Navbar.scss';
 
-const Navbar = ({ children, className }) => (
+export const Navbar = ({ children, className }) => (
 	<div className={className}>{children}</div>
 );
-
-export default Navbar;
 
 /*
 A navigation bar is basically a list of links, so using the <ul> and <li> elements makes perfect sense
