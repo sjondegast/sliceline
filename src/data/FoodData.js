b9f99fb028d84f62805d2f933e6a61eaf525018b
Sjon de Gast <sjondegast@gmail.com>
2020-02-26 21:53:59 +0100

    added foods to data/FoodData.js for menu

--- src/data/FoodData.js
+++ src/data/FoodData.js
@@ -6,5 +6,29 @@ export const foods = [
 	{
 		name: 'Pepperoni Pizza',
 		img: './img/pizza2.jpeg'
+	},
+	{
+		name: 'Chicken Pizza',
+		img: './img/chicken-pizza.jpeg'
+	},
+	{
+		name: 'Veggie Pizza',
+		img: '/img/healthy-pizza.jpeg'
+	},
+	{
+		name: 'Burger',
+		img: '/img/burger.jpeg'
+	},
+	{
+		name: 'Shrimp PoBoy',
+		img: '/img/sandwich.jpeg'
+	},
+	{
+		name: 'Fries',
+		img: '/img/fries.jpeg'
+	},
+	{
+		name: 'Gyro',
+		img: './img/gyro.jpeg'
 	}
 ];
