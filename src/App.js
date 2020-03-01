2ee89e3bc3095ea55959726cc70decad26fc9f1a
Sjon de Gast <sjondegast@gmail.com>
2020-03-01 20:20:17 +0100

    App.js now a class Component React

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar } from './components/Navbar';
 import Link from './components/Link/Link';
 import Banner from './components/Banner/Banner';
 import Menu from './components/Menu/';
 import { Modal } from './components/Modal/index';
 
-function App() {
-	return (
-		<React.Fragment>
-			<Navbar className='navbar'>
-				<Link className='navbar navbar-brand'>
-					SliceLine{' '}
-					<span role='img' aria-label='app logo'>
-						🍕
-					</span>
-				</Link>
-			</Navbar>
-			<Banner
-				background={
-					'https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
-				}
-				classNames='banner'
-			/>
-			<Menu />
-			<Modal show='false'>
-				<div>Hello Modal</div>
-			</Modal>
-		</React.Fragment>
-	);
+class App extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<Navbar className='navbar'>
+					<Link className='navbar navbar-brand'>
+						SliceLine{' '}
+						<span role='img' aria-label='app logo'>
+							🍕
+						</span>
+					</Link>
+				</Navbar>
+				<Banner
+					background={
+						'https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
+					}
+					classNames='banner'
+				/>
+				<Menu />
+				<Modal show='false'>
+					<div>Hello Modal</div>
+				</Modal>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
