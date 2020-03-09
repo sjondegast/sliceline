dad10dbe5ccda4a254d6f373d95df143b79496ce
Sjon de Gast <sjondegast@gmail.com>
2020-03-09 14:25:01 +0100

    added some components for the Modal Component: Banner, Header, Content and Footer

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,1 +1,5 @@
 export { Modal } from './Modal';
+export { ModalHeader } from './ModalHeader';
+export { ModalBanner } from './ModalBanner';
+export { ModalContent } from './ModalContent';
+export { ModalFooter } from './ModalFooter';
